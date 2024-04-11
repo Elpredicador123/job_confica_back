3fde07383050dad748d20a87825f50099196055c
Elpredicador123 <agentepredicador@gmail.com>
2024-04-11 02:06:54 -0500

    fix quqality v3

--- app/Http/Controllers/Api/AuditController.php
+++ app/Http/Controllers/Api/AuditController.php
@@ -17,8 +17,8 @@ class AuditController extends Controller
     public function getMonths()
     {
         try {
-            $months = Audit::selectRaw('CONCAT(audits.AÑO,"-",audits.MES) as MES')
-            ->groupByRaw('CONCAT(audits.AÑO,"-",audits.MES)')
+            $months = Audit::selectRaw('CONCAT(audits.AÑO,"-",LPAD(audits.MES,2,"0")) as MES')
+            ->groupByRaw('CONCAT(audits.AÑO,"-",LPAD(audits.MES,2,"0"))')
             ->get();
             return response()->json([
                 "status" => "success",
